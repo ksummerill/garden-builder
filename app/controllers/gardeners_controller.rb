2d7a46e55e90d52bb5ef259218342f2e9b66c637
ksummerill <kaitlynsummerill@gmail.com>
2020-08-17 20:42:22 -0700

    add create and show to gardeners controller

--- app/controllers/gardeners_controller.rb
+++ app/controllers/gardeners_controller.rb
@@ -3,4 +3,24 @@ class GardenersController < ApplicationController
   def new
     @gardener = Gardener.new
   end
+
+  def create
+    @gardener = Gardener.new(gardener_params)
+    if @gardener.save
+      session[:gardener_id] = @gardener.id
+      redirect_to gardener_path(@gardener)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @gardener = Gardener.find_by_id(params[:id])
+  end
+
+  private
+
+  def gardener_params
+    params.require(:gardener).permit(:username, :password, :first_name, :last_name)
+  end
 end
--- app/models/gardener.rb
+++ app/models/gardener.rb
@@ -2,4 +2,8 @@ class Gardener < ApplicationRecord
   has_many :gardens
   has_many :plants, through: :gardens
   has_many :notes
+
+  validates :username, uniqueness: true, presence: true
+
+  has_secure_password
 end
