04bab8fe2b1526cedc854efde36cdcaabfc4b556
ksummerill <kaitlynsummerill@gmail.com>
2020-08-26 07:38:02 -0700

    plant controller with private and new methods

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,8 +1,9 @@
 class PlantsController < ApplicationController
   before_action :get_garden
+
   # creates a plant object that’s associated with the specific garden instance from the get_garden method
   def new
-    @plant = @garden.plant.build
+    @plant = @garden.plants.build
   end
 
   private
@@ -11,13 +12,13 @@ class PlantsController < ApplicationController
     @garden = Garden.find(params[:garden_id])
   end
 
-  # finds a matching id of a plant in a collection of gardens owned by a particular gardener
+  # finds a matching id of a garden in a collection of gardens owned by a particular gardener
   def set_garden
     @garden = Garden.find(params[:id])
   end
 
   def plant_params
-    params.require(:garden).permit(:name, :variety)
+    params.require(:garden).permit(:name, :amount_of_sun, :water_frequency, :category)
   end
 
 end
