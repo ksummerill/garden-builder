4bdcd59e41121fa48c88fdd8f0b719bac1cc4785
ksummerill <kaitlynsummerill@gmail.com>
2020-08-30 19:13:39 -0700

    nav menu partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :show_menu?
 
     # method for checking current_user
     def current_user
@@ -14,5 +14,9 @@ class ApplicationController < ActionController::Base
       unless current_user
         redirect_to root_url
       end
+    end
+
+    def show_menu?
+      true
     end
 end
