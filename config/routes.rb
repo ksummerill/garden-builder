5bea6249b960917bf4e6f00fbd396c96eea7d043
ksummerill <kaitlynsummerill@gmail.com>
2020-09-05 11:55:59 -0700

    delete action for plants working

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     resources :tasks
   end
 
-  resources :notes
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
